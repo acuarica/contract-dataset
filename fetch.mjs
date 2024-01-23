c3a868b9a51873e4cd5d99c90f932556fba0e6c5
Luis Mastrangelo <luismastrangelo@gmail.com>
2024-01-23 13:04:53 -0300

    Use .env and improve output format

--- fetch.mjs
+++ fetch.mjs
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --env-file=.env
 
 import { Provider } from './provider.mjs';
 import * as path from 'path';
@@ -7,19 +7,24 @@ import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
 // https://gist.github.com/leommoore/4526808
 // https://en.wikipedia.org/wiki/ANSI_escape_code
 const dim = text => `\x1b[2m${text}\x1b[0m`;
+const green = text => `\x1b[32m${text}\x1b[0m`;
+const yellow = text => `\x1b[33m${text}\x1b[0m`;
 const blue = text => `\x1b[34m${text}\x1b[0m`;
 const magenta = text => `\x1b[35m${text}\x1b[0m`;
 const cyan = text => `\x1b[36m${text}\x1b[0m`;
 
 const info = (message, ...optionalParams) => console.info(dim('[info]'), message, ...optionalParams);
 
-const apiKey = readFileSync('./.ALCHEMY_KEY', 'utf-8');
+const {
+    ALCHEMY_KEY = undefined,
+    INFURA_KEY = '84842078b09946638c03157f83405213',
+} = process.env;
 
 const provider = new class {
     providers = [
-        new Provider(`https://eth-mainnet.g.alchemy.com/v2/${apiKey}`),
-        new Provider('https://mainnet.infura.io/v3/84842078b09946638c03157f83405213'),
         new Provider('https://cloudflare-eth.com/'),
+        new Provider(`https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`),
+        new Provider(`https://mainnet.infura.io/v3/${INFURA_KEY}`),
     ];
     current = 0;
 
@@ -37,17 +42,19 @@ async function main() {
         .map(entry => entry.trimEnd().replace(/"/g, '').split(','))
     info(contracts.length, 'contracts');
 
-    const network = 'mainnet';
+    const chainId = (await provider._.chainId()).toString();
 
-    mkdirSync(network, { recursive: true });
-    info('Network', blue(network), `(${magenta(await provider._.chainId())})`, 'at block \uD83D\uDCE6', await provider._.blockNumber());
+    info(`Chain ID ${blue(chainId)} at block \uD83D\uDCE6 ${await provider._.blockNumber()}`);
+
+    mkdirSync(chainId, { recursive: true });
 
     for (const [address, name] of contracts) {
-        const filePath = path.join(network, `${name}-${address}.bytecode`);
+        const filePath = path.join(chainId, `${name}-${address}.bytecode`);
+        process.stdout.write(`Fetching contract ${cyan(name)} ${magenta(address)}`);
         if (existsSync(filePath)) {
-            info(dim(`Contract cached ${cyan(name)} ${magenta(address)}`));
+            console.info(green(' \u2713'));
         } else {
-            info(`Fetching contract ${cyan(name)} ${magenta(address)}`);
+            console.info(yellow(' \u2913'));
             const content = await provider._.getCode(address);
             writeFileSync(filePath, content, 'utf8');
         }
